3a374ddfe8b77d7cd3b0b5215470f1b7ab462da3
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-16 21:52:16 -0300

    Base do Sistema de Artigos

---
+++ controllers/artigos.controller.js
@@ -0,0 +1,1 @@
+// Regras de negócio do tema de artigos
--- index.js
+++ index.js
@@ -2,47 +2,12 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-// MVC (model, view, controller)
-
-// GET - obter algo
-// POST - criar algo
-// PUT - atualizar algo
-// DELETE - deletar algo
-
-// npm install --save-dev nodemon
-
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.send("Minha primeira requisição");
-});
-
-app.get("/segunda-req", function(req, res) {
-    res.send("Minha SEGUNDA requisição"); 
-});
-
-app.get("/com-parametros", function (req, res) {
-    if (req.query.nome === "Caroline") {
-        req.send("Caroline chamou requisição");
-    }
-    res.send("Com parâmetros funciona! Sabadou " + req.query.nome + req.query.sobrenome);
-});
-
-app.post("/meu-primeiro-post", function (req, res) {
-    console.log(req.body);
-    res.send("Meu post funciona");
-});
-
-app.put("/meu-primeiro-put/:id", function (req, res) {
-    console.log(req.body, req.params.id);
-    res.send("Meu put funciona");
-});
-
-app.delete("/meu-primeiro-delete/:id", function(req, res) {
-    console.log(req.params.id);
-    res.send("Meu delete funciona" + req.params.id);
+    res.send("Dasa Educa - Artigos");
 });
 
 app.listen(port, function() {
-    console.log("Ouvindo a porta: ", port);
+    console.log("Ouvindo a porta ", + port);
 });
