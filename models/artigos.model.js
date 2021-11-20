9b62cde05e9d1052a9c50e60bfd98489d53f7542
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-20 13:32:24 -0300

    Aula 11

--- models/artigos.model.js
+++ models/artigos.model.js
@@ -4,16 +4,20 @@
 
 module.exports = (sequelizeDatabase, Sequelize) => {
     const Artigo = sequelizeDatabase.define("artigos", {
-        titulo: {
-            type: Sequelize.STRING
-        },
-        descricao: {
-            type: Sequelize.STRING
-        },
-        publicado: {
-            type: Sequelize.BOOLEAN
-        }
+      titulo: {
+        type: Sequelize.STRING,
+      },
+      descricao: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "Artigo em construção...",
+      },
+      publicado: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
     });
-
+  
     return Artigo;
-}
+  };
