8c0052721ac22c921c10c87269b86d0204774bea
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-20 11:16:45 -0300

    Aula 11

--- controllers/artigos.controller.js
+++ controllers/artigos.controller.js
@@ -4,29 +4,79 @@ const tabelaArtigos = database.artigos;
 
 // Cria um novo artigo
 exports.create = (request, response) => {
-    const artigo = {
-        titulo: request.body.titulo,
-        descricao: request.body.descricao,
-        publicado: request.body.publicado
-    };
+  const artigo = {
+    titulo: request.body.titulo,
+    descricao: request.body.descricao,
+    publicado: request.body.publicado,
+  };
+  // a promise pode ser resolvida
+  // ou ela pode ser rejeitada (exemplo: ocorreu um erro ao tentar salvar)
 
-    // a promise pode ser resolvida
-    // ou ela pode ser rejeitada (exemplo: ocorreu um erro ao tentar salvar)
-
-    tabelaArtigos.create(artigo)
+  tabelaArtigos
+    .create(artigo)
     .then(function () {
-        response.send("Artigo criado com sucesso");
+      response.send("Artigo criado com sucesso");
     })
     .catch(function (error) {
-      console.log(error);
-      response.status(500).send("Ocorreu um erro ao salvar o artigo");
-    })
+      response
+        .status(500)
+        .send({ message: "Ocorreu um erro ao salvar o artigo" });
+    });
 };
 
 exports.findAll = (request, response) => {
-    tabelaArtigos.findAll().then(function (data) {
+  tabelaArtigos
+    .findAll()
+    .then(function (data) {
+      response.send(data);
+    })
+    .catch(function () {
+      response
+        .status(500)
+        .send({ message: "Ocorreu um erro aos buscar todos os artigos." });
+    });
+};
+
+exports.findByTitle = (request, response) => {
+  const tituloArtigo = request.query.titulo;
+  tabelaArtigos
+    .findOne({ where: { titulo: tituloArtigo } })
+    .then(function (data) {
+      if (data) {
         response.send(data);
-    }).catch(function () {
-        response.status(500).send("Ocorreu um erro ao buscar os artigos");
+      } else {
+        response.status(404).send({
+          message:
+            "Não foi possível encontrar um artigo com o titulo: " +
+            tituloArtigo,
+        });
+      }
+    })
+    .catch(function () {
+      response.status(500).send({
+        message:
+          "Ocorreu um erro ao buscar o artigo com titulo: " + tituloArtigo,
+      });
+    });
+};
+
+exports.findById = (request, response) => {
+  const idArtigo = request.query.id;
+  tabelaArtigos
+    .findByPk(idArtigo)
+    .then(function (data) {
+      if (data) {
+        response.send(data);
+      } else {
+        response.status(404).send({
+          message: "Não foi possível encontrar um artigo com o id: " + idArtigo,
+        });
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      response.status(500).send({
+        message: "Ocorreu um erro ao buscar o artigo com titulo: " + idArtigo,
+      });
     });
 };
--- routes/artigos.routes.js
+++ routes/artigos.routes.js
@@ -25,8 +25,14 @@ module.exports = (app) => {
     */
   
     router.post("/", artigosController.create);
-
-    router.get ("/", artigosController.findAll);
+  
+    router.get("/", artigosController.findAll);
+  
+    router.get("/findByTitle", artigosController.findByTitle);
+  
+    router.get("/findById", artigosController.findById);
+  
+    // router.get("/findById/:id", artigosController.findByPk);
   
     app.use("/artigos", router);
   };
